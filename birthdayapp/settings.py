1ed6954ffcda986ec7a20c187efc76aa1366040a
rameshwar <rameshwarvarma97@gmail.com>
2022-01-08 23:38:23 +0530

    Second Commit

--- birthdayapp/settings.py
+++ birthdayapp/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--6)cr^7x9ru-fo-_69a!86b1-hliu8)f92#v3s1!n5s6zi&wn$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['birthdaywishesapp.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
